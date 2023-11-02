ead0c416f7f31add6c112828a4429004a0635cc6
Mareike Zins <fuerzeuch@proton.me>
2023-11-02 19:32:53 +0100

    Status Week 7 Mid Lesson 8 Added Weather Data

--- src/app.js
+++ src/app.js
@@ -1,11 +1,41 @@
 function refreshWeather(response) {
     console.log(response.data);
     let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
+
     temperatureElement.innerHTML = Math.round(
         response.data.temperature.current
     );
-    let cityElement = document.querySelector("#city");
     cityElement.innerHTML = response.data.city;
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = response.data.temperature.humidity + "%";
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+    timeElement.innerHTML = formatDate(date);
+}
+
+function formatDate(date) {
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+    ];
+    let day = days[date.getDay()];
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    // Tuesday 18:00
+    return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
