a84ffffae4a588e9ee904c9532c35ce25527f76a
Mareike Zins <fuerzeuch@proton.me>
2023-11-02 18:54:07 +0100

    Status after Week 7 Lesson 7 API integration

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
+function refreshWeather(response) {
+    console.log(response.data);
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(
+        response.data.temperature.current
+    );
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+    let apiKey = "bd6b645te7b552aa0f390e2137b8oe0e";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    console.log(searchInput.value);
-
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+// Search for Miami as default city when loading the page
+searchCity("Miami");
